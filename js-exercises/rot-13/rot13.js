1d72cd766d09ed7d0a00916e4e57edd511619423
uditbh <udit.bhargav@gmail.com>
2020-09-22 18:19:41 +0530

    Update inner function name

--- js-exercises/rot-13/rot13.js
+++ js-exercises/rot-13/rot13.js
@@ -4,7 +4,7 @@ function rot13(...args) {
   for (let i = 0; i < args.length; i++) {
     let asciiCode = args[i].charCodeAt();
     if (asciiCode <= 90 && asciiCode >= 65) {
-      decryptedString += rotDecode(asciiCode);
+      decryptedString += asciiToChar(asciiCode);
     } else {
       decryptedString += args[i];
     }
@@ -12,7 +12,7 @@ function rot13(...args) {
   return decryptedString;
 }
 
-function rotDecode(asciiCode) {
+function asciiToChar(asciiCode) {
   let places = 13;
   if (asciiCode >= (65 + places)) {
     asciiCode -= places;
